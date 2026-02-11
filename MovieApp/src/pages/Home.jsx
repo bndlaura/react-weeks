3b5fedb862d23d14a76db2bb271ab3bb2f022bc4
Laura Bondor <Laura.Bondor@cognizant.com>
2026-02-11 10:45:50 +0200

    SearchFilters functionality

--- MovieApp/src/pages/Home.jsx
+++ MovieApp/src/pages/Home.jsx
@@ -1,14 +1,47 @@
+import { useState } from "react";
 import movies from "../data/movies.json";
 import MovieList from "../components/MovieList/MovieList.jsx";
 import SearchFilters from "../components/SearchFilters/SearchFilters.jsx";
 
 function Home() {
+  const [searchMovie, setSearchMovie] = useState("");
+  const [genre, setGenre] = useState("");
+  const [rating, setRating] = useState("");
+  const [sort, setSort] = useState("none");
+
+  const filteredMovies = movies.filter((movie) => {
+    const matchesSearch = movie.title.toLowerCase().includes(searchMovie.toLowerCase()) ;
+    const matchesGenre = genre ? movie.genre.toLowerCase() === genre.toLowerCase() : true;
+    const matchesRating =
+      rating === "8" ? movie.rating >= 8 :
+      rating === "5-7" ? movie.rating >= 5 && movie.rating < 8 :
+      rating === "sub5" ? movie.rating < 5 : true;
+    return matchesSearch && matchesGenre && matchesRating;
+  })
+  .sort((a, b) => {
+    if (sort === "asc") return a.title.localeCompare(b.title);
+    if (sort === "desc") return b.title.localeCompare(a.title);
+    return 0;
+  });
+
   return (
     <div className="home">
-      <SearchFilters />
-      <MovieList movies={movies} />
+      <SearchFilters 
+      searchMovie={searchMovie} 
+      genre={genre} 
+      rating={rating} 
+      sort={sort} 
+      setSearchMovie={setSearchMovie} 
+      setGenre={setGenre} 
+      setRating={setRating} 
+      setSort={setSort} 
+      />
+      {filteredMovies.length === 0 ? (
+        <p className="no-results">No movies found matching your criteria.</p>
+      ) : (
+        <MovieList movies={filteredMovies} />
+      )}
     </div>
-   
   );
 }
 
