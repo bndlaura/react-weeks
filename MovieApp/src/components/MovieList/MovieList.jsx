1daf4e48765f1ed3344efd4984c75ed0393ab5c1
Laura Bondor <Laura.Bondor@cognizant.com>
2026-02-11 11:45:57 +0200

    Watchlist&LocalStorage

--- MovieApp/src/components/MovieList/MovieList.jsx
+++ MovieApp/src/components/MovieList/MovieList.jsx
@@ -1,11 +1,16 @@
 import MovieCard from "../MovieCard/MovieCard.jsx";
 import "./MovieList.css";
 
-function MovieList({ movies }) {
+function MovieList({ movies, watchlist, toggleWatchlist }) {
   return (
     <div className="movie-list">
       {movies.map(movie => (
-        <MovieCard key={movie.id} movie={movie} />
+        <MovieCard 
+          key={movie.id} 
+          movie={movie} 
+          isAdded={watchlist.some(m => m.id === movie.id)} 
+          toggleWatchlist={toggleWatchlist} 
+        />
       ))}
     </div>
   );
--- MovieApp/src/pages/Watchlist.jsx
+++ MovieApp/src/pages/Watchlist.jsx
@@ -1,9 +1,39 @@
+import { useState } from "react";
+import MovieList from "../components/MovieList/MovieList";
 
+function Watchlist() {
+  const [watchlist, setWatchlist] = useState(() => {
+    return JSON.parse(localStorage.getItem("watchlist")) || [];
+  });
 
-export default function Watchlist() {
-    return (
-        <div className="watchlist">
-            <h1>Welcome to the Watchlist Page</h1>
-        </div>
-    )
+  function toggleWatchlist(movie) {
+    const exists = watchlist.some((m) => m.id === movie.id);
+
+    let updated;
+    if (exists) {
+      updated = watchlist.filter((m) => m.id !== movie.id);
+    } else {
+      updated = [...watchlist, movie];
+    }
+
+    setWatchlist(updated);
+    localStorage.setItem("watchlist", JSON.stringify(updated));
+  }
+
+  return (
+    <div className="watchlist-page">
+ 
+      {watchlist.length === 0 ? (
+        <p className="no-results">No movies added yet.</p>
+      ) : (
+        <MovieList
+          movies={watchlist}
+          watchlist={watchlist}
+          toggleWatchlist={toggleWatchlist}
+        />
+      )}
+    </div>
+  );
 }
+
+export default Watchlist;
